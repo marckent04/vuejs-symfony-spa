9884641f4ee43b133fb515a1cfff2b267fc651ed
Nanguy <marchenrynanguy1@gmail.com>
2020-03-21 11:45:50 +0000

    vadding bootstrap-vue + vue icons

--- assets/vue/index.js
+++ assets/vue/index.js
@@ -3,7 +3,11 @@ import App from "./App";
 import router from "./router";
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 Vue.use(VueAxios, axios)
 
 new Vue({
--- assets/vue/pages/Home.vue
+++ assets/vue/pages/Home.vue
@@ -6,6 +6,15 @@
 
         <div class="row col">
             <p>This is the homepage of our Vue.js application.</p>
+            <b-icon icon="arrow-up"></b-icon>
+            <div class="text-center">
+                <b-spinner label="Spinning"></b-spinner>
+                <b-spinner type="grow" label="Spinning"></b-spinner>
+                <b-spinner variant="primary" label="Spinning"></b-spinner>
+                <b-spinner variant="primary" type="grow" label="Spinning"></b-spinner>
+                <b-spinner variant="success" label="Spinning"></b-spinner>
+                <b-spinner variant="success" type="grow" label="Spinning"></b-spinner>
+            </div>
         </div>
     </div>
 </template>
